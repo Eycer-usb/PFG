39493b7df658ea5e3a65a9861a038ac0b701560d
Eros CR <16-10216@usb.ve>
2024-04-13 19:01:58 -0400

    Maven Instalation

--- Code/postgres/installer/install_dependencies.sh
+++ Code/postgres/installer/install_dependencies.sh
@@ -44,4 +44,29 @@ if [ ${client} = true ]; then
     sudo apt install -y openjdk-17-jdk \
     openjdk-17-jre \
     bc
+
+    # Maven Instalation
+    mvn -version
+    if [ ! $? -eq  0 ]; then
+      wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
+      sudo mkdir -p /usr/local/apache-maven/
+      sudo tar -xvf apache-maven-3.9.6-bin.tar.gz -C /usr/local/apache-maven
+      echo "export M2_HOME=/usr/local/apache-maven/apache-maven-3.9.6" >> ~/.bashrc
+      echo "export M2=$M2_HOME/bin" >> ~/.bashrc
+      echo "export MAVEN_OPTS=-Xms256m -Xmx512m" >> ~/.bashrc
+      echo "export PATH=$M2:$PATH" >> ~/.bashrc
+      source ~/.bashrc
+      mvn -version
+      if [ ! $? -eq  0 ]; then
+        echo "Error Installing Maven"
+        exit 1
+      else
+        rm apache-maven-3.9.6-bin.tar.gz
+      fi
+    else
+      echo "Maven Already Installed"
+    fi
+
+
+
 fi
