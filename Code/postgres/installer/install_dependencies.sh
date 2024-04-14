9e8f45f298204641b4c1b920ebcbf64641e608ec
Eros CR <eycer1@gmail.com>
2024-04-14 13:28:55 -0400

    Updating Installation Script

--- Code/postgres/installer/install_dependencies.sh
+++ Code/postgres/installer/install_dependencies.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
 server=false
 client=false
-
 
 # Usage Instructions
 print_usage() {
@@ -15,58 +13,57 @@ print_usage() {
 }
 
 if [[ $# -eq 0 ]]; then
-    print_usage
-    exit 1
+  print_usage
+  exit 1
 fi
 
 # Options Processor
 while getopts 'csh' flag; do
   case "${flag}" in
-    c) client=true ;;
-    s) server=true ;;
-    h) print_usage
-       exit 1 ;;
+  c) client=true ;;
+  s) server=true ;;
+  h)
+    print_usage
+    exit 1
+    ;;
+  *) print_usage
+    exit 1 ;;
   esac
 done
 
 # Server Side Dependencies Instalation
 if [ ${server} = true ]; then
-    sudo apt install -y postgresql-common \
-        postgresql-client-14 \
-        postgresql-14 \
-        postgresql-server-dev-14 \
-        postgresql-contrib \
-        postgresql-server-dev-all
+  sudo apt install -y postgresql-common \
+    postgresql-client-14 \
+    postgresql-14 \
+    postgresql-server-dev-14 \
+    postgresql-contrib \
+    postgresql-server-dev-all
 fi
 
 # Client Side Dependencies Instalation
 if [ ${client} = true ]; then
-    sudo apt install -y openjdk-17-jdk \
+  sudo apt install -y openjdk-17-jdk \
     openjdk-17-jre \
     bc
 
-    # Maven Instalation
+  # Maven Instalation
+  mvn -version
+  if [ ! $? -eq 0 ]; then
+    wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
+    sudo mkdir -p /usr/local/apache-maven/
+    sudo tar -xvf apache-maven-3.9.6-bin.tar.gz -C /usr/local/apache-maven
+    echo "export M2_HOME=/usr/local/apache-maven/apache-maven-3.9.6" >>~/.bashrc
+    echo 'export M2=$M2_HOME/bin' >>~/.bashrc
+    echo 'export MAVEN_OPTS=-"Xms256m -Xmx512m"' >>~/.bashrc
+    echo 'export PATH=$M2:$PATH' >>~/.bashrc
     mvn -version
-    if [ ! $? -eq  0 ]; then
-      wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
-      sudo mkdir -p /usr/local/apache-maven/
-      sudo tar -xvf apache-maven-3.9.6-bin.tar.gz -C /usr/local/apache-maven
-      echo "export M2_HOME=/usr/local/apache-maven/apache-maven-3.9.6" >> ~/.bashrc
-      echo "export M2=$M2_HOME/bin" >> ~/.bashrc
-      echo "export MAVEN_OPTS=-Xms256m -Xmx512m" >> ~/.bashrc
-      echo "export PATH=$M2:$PATH" >> ~/.bashrc
-      source ~/.bashrc
-      mvn -version
-      if [ ! $? -eq  0 ]; then
-        echo "Error Installing Maven"
-        exit 1
-      else
-        rm apache-maven-3.9.6-bin.tar.gz
-      fi
-    else
-      echo "Maven Already Installed"
+    if [ ! $? -eq 0 ]; then
+      echo "Error Installing Maven try reloading the terminal or check the installation manually"
     fi
-
-
+    rm apache-maven-3.9.6-bin.tar.gz
+  else
+    echo "Maven Already Installed"
+  fi
 
 fi
