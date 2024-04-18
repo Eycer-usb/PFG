fad172e785dc909096ae29d252670f3c4788e178
Eros CR <16-10216@usb.ve>
2024-04-17 23:05:02 -0400

    Adding dependencies to postgres-executor

--- Code/postgres/start.sh
+++ Code/postgres/start.sh
@@ -5,7 +5,7 @@ export $(grep -v '^#' environment.env |  xargs -d '\n')
 cd executor
 mvn clean install package
 cd ..
-cp executor/target/*-executor-*.jar orchestrator/$jar_file
+cp executor/target/*-executor-*jar-with-dependencies.jar orchestrator/$jar_file
 
 cd orchestrator
 sudo -E ./run.sh
