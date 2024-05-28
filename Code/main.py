b4dd1cf4757909b811af75637ad0d2c721ccfa88
Eros CR <16-10216@usb.ve>
2024-05-27 22:59:24 -0400

    Collector from main scrypt

--- Code/main.py
+++ Code/main.py
@@ -1,14 +1,15 @@
 import sys
 import postgres.console.client as postgres_client
 import postgres.console.server as cserver
+import projects.collector.collector as collector
 import projects.observer.observer as lib
 import json
 
 def print_help():
     print("Required at least three arguments")
+    print("config.json  --- File with configuration")
     print("client or server  --- Run as client or server")
     print("postgres or mongo  --- Run postgres or mongo")
-    print("config.json  --- File with configuration")
 
 
 def client_postgres(config):
@@ -23,6 +24,12 @@ def client_postgres(config):
 def server_postgres(config):
     pass
 
+def set_analyst(config):
+    pass
+
+def set_collector(config):
+    collector.prepare_resources(config)
+    return collector.start(config)
 
 def get_config(filepath):
     config = {}
@@ -32,43 +39,60 @@ def get_config(filepath):
         
 
 if __name__ == '__main__':
-    # Input Validation
-    if(len(sys.argv) <= 3):
-        print_help()
-        exit(-1)
-    elif(not (sys.argv[1] == "client" or sys.argv[1] == "server" )):
-        print_help()
-        exit(-1)
-    elif(not (sys.argv[2] == "postgres" or sys.argv[1] == "mongo" )):
-        print_help()
-        exit(-1)
+    # Options Available
+    options = [
+        "client", "server", "analyst", "collector"
+    ]
+    observable_options = [
+        "client", "server"
+    ]
 
+    # Input Validation
+    if(len(sys.argv) <= 2):
+        print_help()
+        exit(-1)
+    elif(not (sys.argv[2] in options )):
+        print_help()
+        exit(-1)
     
-    config = get_config(sys.argv[3]) # Retrieval configuration from file
+    config = get_config(sys.argv[1]) # Retrieval configuration from file
     
     # Setting up observer configuration
     observer_config = {
         "observer_port": "",
-        "collector_endpoint": config["collector"]["metrics"]
+        "collector_endpoint": ""
     }
 
     # Client Process
     client_process = None
 
     # Preparing and running orchestrators in each case
-    if(sys.argv[1] == "client" and sys.argv[2] == "postgres" ):
+    if(sys.argv[2] == "collector"):
+        print("Running as Collector")
+        client_process = set_collector(config)
+    elif(sys.argv[2] == "analyst"):
+        print("Running as Analyst")
+        set_analyst(config)
+    elif(sys.argv[2] == "client" and sys.argv[3] == "postgres" ):
+        print("Running as Postgres Client")
         observer_config["observer_port"] = config["clientObserver"]["port"]
+        observer_config["collector_endpoint"] = config["collector"]["client-metrics"]
         client_process = client_postgres(config)
-    elif(sys.argv[1] == "server" and sys.argv[2] == "postgres"):
+    elif(sys.argv[2] == "client" and sys.argv[3] == "mongo"):
+        print("Running as Mongo Client")
+    elif(sys.argv[2] == "server" and sys.argv[3] == "postgres"):
+        print("Running as Postgres Server")
         observer_config["observer_port"] = config["serverObserver"]["port"]
+        observer_config["collector_endpoint"] = config["collector"]["server-metrics"]
         server_postgres(config)
-    elif(sys.argv[1] == "client" and sys.argv[2] == "mongo"):
-        pass
-    elif(sys.argv[1] == "server" and sys.argv[2] == "mongo"):
-        pass
+    elif(sys.argv[2] == "server" and sys.argv[3] == "mongo"):
+        print("Running as Mongo Server")
 
     # Starting observer
-    observer_process = lib.start_observer(observer_config,
+    if(sys.argv[2] in observable_options):
+        observer_process = lib.start_observer(observer_config,
                                           console_command=config["consoleCommand"])
-    observer_process.wait()
-    client_process.wait()
+        observer_process.wait()
+        
+    if(sys.argv[2] == "client"):
+        client_process.wait()
