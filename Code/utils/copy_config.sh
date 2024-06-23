1f129818042066ad239eaaf82375dd707b556b13
Eros CR <16-10216@usb.ve>
2024-06-23 13:15:39 -0400

    Adding copy script

--- Code/utils/copy_config.sh
+++ Code/utils/copy_config.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 source environment.env
 IP_START=159.90.9
-START=3
-END=19
 USERNAME=ec
+
+scp ./config_pb.json $USERNAME@${IP_START}.3:~/PFG/Code/config.json
+scp ./config_pb.json $USERNAME@${IP_START}.7:~/PFG/Code/config.json
+
+scp ./config_pi.json $USERNAME@${IP_START}.4:~/PFG/Code/config.json
+scp ./config_pi.json $USERNAME@${IP_START}.8:~/PFG/Code/config.json
+
+scp ./config_pc.json $USERNAME@${IP_START}.5:~/PFG/Code/config.json
+scp ./config_pc.json $USERNAME@${IP_START}.9:~/PFG/Code/config.json
 
 scp ./config_pic.json $USERNAME@${IP_START}.6:~/PFG/Code/config.json
 scp ./config_pic.json $USERNAME@${IP_START}.10:~/PFG/Code/config.json
