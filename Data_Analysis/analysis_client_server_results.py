9736e208524b6658f18afeaf40795ccbefee705d
Eros CR <16-10216@usb.ve>
2024-08-03 17:17:25 -0400

    Adding Plots

--- Data_Analysis/analysis_client_server_results.py
+++ Data_Analysis/analysis_client_server_results.py
@@ -1,7 +1,8 @@
 import sys
 import pandas as pd
 import scipy.stats as stats
-
+from tabulate import tabulate
+import matplotlib.pyplot as plt
 
 def get_optimizations(df):
     return df['optimization_key'].unique()
@@ -59,7 +60,10 @@ def apply_analysis(df):
 def make_plots(df):
     pass
 
-def main(filename):
+    
+    
+
+def analysis(filename):
     df = pd.read_csv(filename, dtype={
     "id": "int64",
     "database_key": "string",
@@ -75,15 +79,113 @@ def main(filename):
     print("Starting Analysis to Postgres results...")
     postgres_results = apply_analysis(df[df['database_key'] == 'postgres'])
 
-    postgres_results.to_csv("postgres_results.csv", index=False)
-    
     
     print("Starting Analysis to Mongo results...")
     mongo_results = apply_analysis(df[df['database_key'] == 'mongodb'])
-    postgres_results.to_csv("mongo_results.csv", index=False)
-
     
+    postgres_results.to_csv("postgres_results.csv", index=False)
+    mongo_results.to_csv("mongo_results.csv", index=False)
+    
+    return df, postgres_results, mongo_results
+
+
+def plot_queries_energy_comparation(optimization_key, database_key, results):
+    values_client = []
+    values_server = []
+    filtered_results = results.loc[ \
+                ((df['database_key'] == database_key) &\
+                    (df['optimization_key'] == optimization_key) &\
+                    (df['client_energy_consumed'] > 0) &\
+                    (df['client_energy_consumed'] != 0) &\
+                    (df['server_energy_consumed'] != 0) &\
+                    (df['server_energy_consumed'] > 0)
+                        )]
+    key = [i for i in range(1,23)]
+    for i in key:
+        result_query = filtered_results.loc[filtered_results['query_key'] == i]
+        values_client.append(result_query['client_energy_consumed'].mean() / 1000)
+        values_server.append(result_query['server_energy_consumed'].mean() / 1000000)
+    
+    # Graficando ambos resultados en el mismo lienzo
+    figure, axis = plt.subplots(1, 2, constrained_layout=True)
+    figure.tight_layout(w_pad=5, h_pad=5)
+    figure.subplots_adjust(top=0.8)
+    figure.suptitle('{} database - Optimization {}'.format(database_key.capitalize(), optimization_key), y=0.98)
+    axis[0].bar(key, values_client, color = 'b')
+    axis[0].set_title('Client Energy Consumed optimization')
+    axis[0].set_xlabel('Query')
+    axis[0].set_ylabel('Energy Consumed (J)')
+    axis[0].set_ylim(0, 50)
+    
+    axis[1].bar(key, values_server, color = 'r')
+    axis[1].set_title('Server Energy Consumed')
+    axis[1].set_xlabel('Query')
+    axis[1].set_ylabel('Energy Consumed (KJ)')
+    
+    plt.savefig('figures/{}-{}-queries_energy_comparation.png'.format(database_key, optimization_key))
+    
+    
+        
+        
+
+def plot_mean_energy_by_optimization(database_key, results):
+    values_client = []
+    values_server = []
+    colors = ['b', 'r', 'g', 'y']
+    optimizations = get_optimizations(results)
+    
+    for optimization_key in optimizations:
+        filtered_results = results.loc[ \
+                ((df['database_key'] == database_key) &\
+                    (df['optimization_key'] == optimization_key) &\
+                    (df['client_energy_consumed'] > 0) &\
+                    (df['client_energy_consumed'] != 0) &\
+                    (df['server_energy_consumed'] != 0) &\
+                    (df['server_energy_consumed'] > 0)
+                        )]
+        values_client.append(filtered_results['client_energy_consumed'].mean() / 1000)
+        values_server.append(filtered_results["server_energy_consumed"].mean()/1000000)
+    # Graficando ambos resultados en el mismo lienzo
+    figure, axis = plt.subplots(1, 2, constrained_layout=True)
+    figure.tight_layout(w_pad=5, h_pad=5)
+    figure.subplots_adjust(top=0.8)
+    figure.suptitle('{} database'.format(database_key.capitalize()), y=0.98)
+    for i, optimization in enumerate(optimizations):
+        axis[0].bar(optimization, values_client[i], color = colors[i], label = optimization)
+        axis[1].bar(optimization, values_server[i], color = colors[i], label = optimization)
+        
+    axis[0].set_title('Client Energy Consumed')
+    axis[0].set_xlabel('Optimization')
+    axis[0].set_ylabel('Mean Energy Consumed (J)')
+    axis[0].get_xaxis().set_visible(False)
+    axis[0].legend()
+    axis[0].set_ylim(0,50)
+    
+    axis[1].set_title('Server Energy Consumed')
+    axis[1].set_xlabel('Optimization')
+    axis[1].set_ylabel('Mean Energy Consumed (KJ)')
+    axis[1].get_xaxis().set_visible(False)
+    axis[1].legend()
+    plt.savefig('figures/{}-means_energy_by_optimization.png'.format(database_key))
+    
+
+
+
+def plots(results, postgres_analysis, mongo_analysis):
+    print("Generating plots...")
+    optimizations = get_optimizations(df)
+    for optimization_key in optimizations:
+        plot_queries_energy_comparation(optimization_key, "postgres", results)
+        
+    for optimization_key in optimizations:
+            plot_queries_energy_comparation(optimization_key, "mongodb", results)
+
+    plot_mean_energy_by_optimization("postgres", results)
+    plot_mean_energy_by_optimization("mongodb", results)
+
+
 
 if "__main__" == __name__:
     file_name = sys.argv[1]
-    main(file_name)
+    df, postgres_results, mongo_results = analysis(file_name)
+    plots(df, postgres_results, mongo_results)
